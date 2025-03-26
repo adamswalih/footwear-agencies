c6960e2b9b0b20fd61282405ffd4a557ebd42c8f
adamswalih <adam.swalih@icloud.com>
2025-03-26 22:52:31 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Sample cart functionality
-let cart = [];
-
-function addToCart(productName, price) {
-  cart.push({ name: productName, price: price });
-  alert(`${productName} added to cart!`);
-  console.log("Cart:", cart); // Check in browser console
-}
